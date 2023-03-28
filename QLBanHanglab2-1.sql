e21f9c81b19a044a892c81bf6a54589ee5a53e8c
Miraclee <75416987+kienthepoor@users.noreply.github.com>
2023-03-28 14:21:18 +0700

    Lab 2.1 tuan 2

---
+++ QLBanHanglab2-1.sql
@@ -0,0 +1,50 @@
+﻿USE QLBanHang
+1. select * from Sanpham, Hangsx, Nhanvien, Nhap, Xuat
+
+
+2. select masp, tensp, tenhang, soluong, mausac, giaban, donvitinh, mota from Sanpham ORDER BY giaban DESC
+
+
+3. select * from Sanpham WHERE hangsx='Samsung'
+
+
+4. select * from sv where gt=N'Nữ' and phong='Kế toán'
+
+
+5. select sohdn, masp, tensp, tenhang, soluongnhap, dongianhap, soluongnhap*dongianhap as tiennhap, mausac, donvitinh, mota, ngay nhap, tennv, phong from Sanpham, Nhanvien, Nhap ORDER BY sohdn ASC
+
+
+6. select sohdx, masp, tensp, tenhang, soluongxuat, giaban, soluongnhap*giaban as tienxuat, mausac, donvitinh, mota, ngayNhap, tennv, phong from Sanpham, Nhanvien, Xuat WHERE ngaynhap='10-2018' ORDER BY sohdx ASC
+
+
+7. select sohdn, masp, tensp, soluongnhap, dongianhap, ngaynhap, Manv, tennv, Phong from Nhanvien, Sanpham, Nhap WHERE ngaynhap='2017'
+
+
+8. select top 10 sohdx, masp, manv, ngayxuat, soluongxuat from Xuat WHERE ngayxuat='10-2018' ORDER BY soluongxuat DESC
+
+
+9. select top 10 masp, tensp, tenhang, soluong, mausac, giaban, donvitinh, mota ORDER BY giaban DESC
+
+
+10. select * from Sanpham WHERE giaban >= 100000 and giaban <= 500000 and tenhang='Samsung'
+
+
+11. select SUM(dongianhap) as "Tong so tien " from Nhap, Hangsx WHERE tenhang = 'Samsung'
+
+
+12. 
+
+
+13. select sohdn, ngaynhap, SUM(dongianhap) as tongtiennhap,  MAX(tongtiennhap) from Nhap WHERE ngaynhap='2018'
+
+
+14. select top 10 masp, tensp, soluongnhap, dongianhap from Sanpham, Hangsx ORDER BY soluongnhap 
+
+
+15. select	masp, tensp, tenhang, manv, tennv from Sanpham, Nhap WHERE manv='NV01' and tenhang='Samsung'
+
+
+16. select sohdn, masp, soluongnhap, ngaynhap, manv, tennv from Sanpham, Nhap WHERE manv='NV02' and masp='SP02'
+
+
+17. select manv, tennv, sohdx, tensp, ngayxuat from Xuat, Sanpham, Nhanvien WHERE manv='SP02' and ngayxuat='03-02/2020'
