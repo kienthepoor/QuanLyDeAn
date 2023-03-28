365a924a27ba2b346302e8644d476127a688b518
Miraclee <75416987+kienthepoor@users.noreply.github.com>
2023-03-28 17:04:35 +0700

    Update QLBanHanglab2-1.sql

--- QLBanHanglab2-1.sql
+++ QLBanHanglab2-1.sql
@@ -11,16 +11,16 @@
 4. select * from sv where gt=N'Nữ' and phong='Kế toán'
 
 
-5. select sohdn, masp, tensp, tenhang, soluongnhap, dongianhap, soluongnhap*dongianhap as tiennhap, mausac, donvitinh, mota, ngay nhap, tennv, phong from Sanpham, Nhanvien, Nhap ORDER BY sohdn ASC
+5. select sohdn, masp, tensp, tenhang, soluongnhap, dongianhap, soluongnhap*dongianhap as tiennhap, mausac, donvitinh, mota, ngaynhap, tennv, phong from Sanpham, Nhanvien, Nhap ORDER BY sohdn ASC
 
 
-6. select sohdx, masp, tensp, tenhang, soluongxuat, giaban, soluongnhap*giaban as tienxuat, mausac, donvitinh, mota, ngayNhap, tennv, phong from Sanpham, Nhanvien, Xuat WHERE ngaynhap='10-2018' ORDER BY sohdx ASC
+6. select sohdx, masp, tensp, tenhang, soluongxuat, giaban, soluongxuat*giaban as tienxuat, mausac, donvitinh, mota, ngayxuat, tennv, phong from Sanpham, Nhanvien, Xuat WHERE MONTH(Xuat.ngayxuat)='10' and YEAR(Xuat.ngayxuat)='2018' ORDER BY sohdx ASC
 
 
-7. select sohdn, masp, tensp, soluongnhap, dongianhap, ngaynhap, Manv, tennv, Phong from Nhanvien, Sanpham, Nhap WHERE ngaynhap='2017'
+7. select sohdn, masp, tensp, soluongnhap, dongianhap, ngaynhap, Manv, tennv, Phong from Nhanvien, Sanpham, Nhap WHERE YEAR(Nhap.ngaynhap)='2017'
 
 
-8. select top 10 sohdx, masp, manv, ngayxuat, soluongxuat from Xuat WHERE ngayxuat='10-2018' ORDER BY soluongxuat DESC
+8. select top 10 sohdx, masp, manv, ngayxuat, soluongxuat from Xuat WHERE YEAR(Xuat.ngayxuat)='2018' ORDER BY soluongxuat DESC
 
 
 9. select top 10 masp, tensp, tenhang, soluong, mausac, giaban, donvitinh, mota ORDER BY giaban DESC
@@ -35,7 +35,7 @@
 12. 
 
 
-13. select sohdn, ngaynhap, SUM(dongianhap) as tongtiennhap,  MAX(tongtiennhap) from Nhap WHERE ngaynhap='2018'
+13. select sohdn, ngaynhap, SUM(dongianhap) as tongtiennhap,  MAX(tongtiennhap) from Nhap WHERE YEAR(Nhap.ngaynhap)='2018'
 
 
 14. select top 10 masp, tensp, soluongnhap, dongianhap from Sanpham, Hangsx ORDER BY soluongnhap 
@@ -47,4 +47,4 @@
 16. select sohdn, masp, soluongnhap, ngaynhap, manv, tennv from Sanpham, Nhap WHERE manv='NV02' and masp='SP02'
 
 
-17. select manv, tennv, sohdx, tensp, ngayxuat from Xuat, Sanpham, Nhanvien WHERE manv='SP02' and ngayxuat='03-02/2020'
+17. select manv, tennv, sohdx, tensp, ngayxuat from Xuat, Sanpham, Nhanvien WHERE manv='SP02' and ngayxuat='03-02-2020'
